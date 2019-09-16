329c6eb6152bb90f5f8ef26e8d00d85917c4b7ed
yficklis <35781724+yficklis@users.noreply.github.com>
2019-09-16 17:22:54 -0300

    [UPD] Musicas

--- src/java/br/com/fatecpg/tp/Musica.java
+++ src/java/br/com/fatecpg/tp/Musica.java
@@ -5,14 +5,30 @@
  */
 package br.com.fatecpg.tp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author marlon.santos
  */
-public class Musica{
+public class Musica extends SuperClass{
+    
+    private Banda banda;
+    private Disco disco;
+    private ArrayList<Musica> musicas;
     
     public Musica(int id, String name) {
-      
+        super(id, name);
+        this.banda = new Banda(id, name);
+        /*this.disco = new Disco(id, name);*/
     }
     
+    public ArrayList<Musica> getMusicas(){
+        return this.musicas;
+    }
+    
+    public void addMusica(Musica musica){
+        this.musicas.add(musica);
+    }
+   
 }
